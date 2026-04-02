2a4379a18ff22154fdae6be0a473f2f759aa44ee
K L C Medha Nipuna <136672322+meniklc@users.noreply.github.com>
2026-04-02 07:10:34 +0530

    Delete advanced_rag directory

--- advanced_rag/config.py
+++
@@ -1,7 +0,0 @@
-import os
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-EMBEDDING_MODEL = "text-embedding-3-small"
-LLM_MODEL = "gpt-4o-mini"
-
-CHROMA_COLLECTION = "papers"
--- advanced_rag/retrieval/search.py
+++
@@ -1,9 +0,0 @@
-from llm.embedder import get_embedding
-from vectorstore.chroma_db import query
-
-def search_papers(query_text):
-    embedding = get_embedding(query_text)
-
-    results = query(embedding, k=5)
-
-    return results["documents"]
